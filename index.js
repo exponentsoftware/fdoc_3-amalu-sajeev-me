bf1334666e3f8396a5249047bad1e0c0d4227e43
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-10 15:16:42 +0530

    signup & login

--- index.js
+++ index.js
@@ -80,8 +80,122 @@ newStudent.skills.dataScience.push('SQL');
  * e. Iterate the keys of the student object
  */
 
-console.log(Object.keys(newStudent).length);
-console.log(Object.keys(newStudent).length);
-console.log(Object.values(newStudent.skills).length);
-console.log(!!newStudent.graphicDesign);
-Object.keys(newStudent).forEach(console.log);
+// console.log(Object.keys(newStudent).length);
+// console.log(Object.keys(newStudent).length);
+// console.log(Object.values(newStudent.skills).length);
+// console.log(!!newStudent.graphicDesign);
+// Object.keys(newStudent).forEach(console.log);
+
+
+
+/**
+ * Questions:a, b and c are based on the following two arrays:users and products
+ */
+
+ const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+    ];
+
+const products = [
+{
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+},
+{
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+},
+{
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+}
+];
+
+/**
+ * a. Imagine you are getting the above users collection from a MongoDB database.
+ * a. Create a function called ***signUp*** which allows user to add to the collection. 
+ * If user exists, inform the user that he has already an account.
+ * b. Create a function called ***signIn*** which allows user to sign in to the application
+ */
+
+signUp(users, {
+    _id: 'ghdercfc',
+    username: 'Nora',
+    email: 'nora@thomas.com',
+    password: '123333',
+    createdAt:'17/05/2019 12:00 AM',
+    isLoggedIn: true
+});
+
+signIn(users, 'Martha', '123222');
+
+function signUp(db, user) {
+    const existingUser = db.some(({ username }) => username === user.username);
+    if (existingUser) throw "user already exists";
+    db.push(user);
+}
+
+function signIn(db, username, password) {
+    for (let i = 0; i < db.length; i++){
+        if (db[i].username === username && db[i].password === password) {
+            db[i].isLoggedIn = true;
+            console.log('logged in');
+            return true;
+        };
+    }
+    return false;
+}
