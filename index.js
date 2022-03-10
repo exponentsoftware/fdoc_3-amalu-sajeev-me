ac4ddf01d4b7e8d3afff38fc89f2037ecd5550ef
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-10 14:53:43 +0530

    newStudent

--- index.js
+++ index.js
@@ -23,9 +23,50 @@ const [, , jsScore, reactScore] = scores;
     ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
 ]
 
-console.log(convertArrayToObject(students));
+// console.log(convertArrayToObject(students));
 
 function convertArrayToObject(arr) {
     const obj = arr.map(([name, skills, scores]) => ({ name, skills, scores }));
     return obj;
 }
+
+/**
+ * Copy the student object to newStudent without mutating the original object. 
+ * In the new object add the following ?
+ * Add Bootstrap with level 8 to the front end skill sets
+ * Add Express with level 9 to the back end skill sets
+ * Add SQL with level 8 to the data base skill sets
+ * Add SQL without level to the data science skill sets
+ */
+
+ const student1 = {
+    name: 'David',
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: 'HTML', level: 10 },
+            { skill: 'CSS', level: 8 },
+            { skill: 'JS', level: 8 },
+            { skill: 'React', level: 9 }
+        ],
+        backEnd: [
+            { skill: 'Node',level: 7 },
+            { skill: 'GraphQL', level: 8 },
+        ],
+        dataBase:[
+            { skill: 'MongoDB', level: 7.5 },
+        ],
+        dataScience:['Python', 'R', 'D3.js']
+    }
+}
+
+
+
+
+const newStudent = {...student1};
+newStudent.skills.frontEnd.push({ skill: 'Bootstrap', level: 8 });
+newStudent.skills.backEnd.push({ skill: 'Express', level: 9 });
+newStudent.skills.dataBase.push({ skill: 'SQL', level: 8 });
+newStudent.skills.dataScience.push('SQL');
+
+console.log(newStudent);
